6a6cb4f50b6618bb46c7b54dd45d771eec4b27a1
Nick Spinale <nick@nickspinale.com>
2024-09-22 04:20:41 -0400

    1.A

--- workspaces/root-task/hello/src/main.rs
+++ workspaces/root-task/hello/src/main.rs
@@ -13,6 +13,8 @@ use sel4_root_task::root_task;
 fn main(_bootinfo: &sel4::BootInfoPtr) -> ! {
     sel4::debug_println!("Hello, World!");
 
+    panic!("uh oh");
+
     sel4::debug_println!("TEST_PASS");
 
     sel4::init_thread::suspend_self()
--- workspaces/root-task/hello/test.py
+++ workspaces/root-task/hello/test.py
@@ -8,4 +8,5 @@ from harness import Simulation
 
 with Simulation.from_args() as sim:
     sim.child.expect('Hello, World!', timeout=3)
-    sim.child.expect('TEST_PASS', timeout=1)
+    sim.child.expect('panicked', timeout=1)
+    sim.child.expect('uh oh', timeout=1)
