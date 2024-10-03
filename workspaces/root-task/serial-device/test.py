33d26e790452a3d8c45c027ad0df214073db62f4
Nick Spinale <nick@nickspinale.com>
2024-10-03 14:03:32 -0700

    Strip serial-device example

--- workspaces/root-task/serial-device/test.py
+++ workspaces/root-task/serial-device/test.py
@@ -4,13 +4,7 @@
 # SPDX-License-Identifier: BSD-2-Clause
 #
 
-import time
-
 from harness import Simulation
 
-sim = Simulation.from_args()
-sim.child.expect('echo> ', timeout=5)
-time.sleep(1)
-sim.child.send('xxx')
-sim.child.expect('\[x\]\[x\]\[x\]', timeout=5)
-print()
+with Simulation.from_args() as sim:
+    sim.child.expect('TEST_PASS', timeout=1)
