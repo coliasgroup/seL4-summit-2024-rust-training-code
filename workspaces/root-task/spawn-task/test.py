3b0cf67166d2524efb42669b50bd0e81025b12d9
Nick Spinale <nick@nickspinale.com>
2024-10-05 02:00:54 -0700

    Fix spawn-task initial test

--- workspaces/root-task/spawn-task/test.py
+++ workspaces/root-task/spawn-task/test.py
@@ -6,4 +6,7 @@
 
 from harness import Simulation
 
-Simulation.from_args().simple_test()
+with Simulation.from_args() as sim:
+    sim.child.expect('In root task', timeout=1)
+    sim.child.expect('In child task', timeout=1)
+    sim.child.expect('TEST_PASS', timeout=1)
