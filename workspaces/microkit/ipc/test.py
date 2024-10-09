571247e5ce2ae0996bcd1486412f58d9ac3f6319
Nick Spinale <nick@nickspinale.com>
2024-10-09 02:35:58 -0700

    Fix test expect order

--- workspaces/microkit/ipc/test.py
+++ workspaces/microkit/ipc/test.py
@@ -7,6 +7,6 @@
 from harness import Simulation
 
 with Simulation.from_args() as sim:
-    sim.child.expect('client: initializing', timeout=1)
     sim.child.expect('server: initializing', timeout=1)
+    sim.child.expect('client: initializing', timeout=1)
     sim.child.expect('client: TEST_PASS', timeout=1)
