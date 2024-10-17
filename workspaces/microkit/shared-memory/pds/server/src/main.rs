88c00a21e465d232a2631c0deab0c43d36fb8639
Nick Spinale <nick@nickspinale.com>
2024-10-18 00:56:12 +1100

    11.A

--- workspaces/microkit/shared-memory/pds/server/src/main.rs
+++ workspaces/microkit/shared-memory/pds/server/src/main.rs
@@ -7,6 +7,10 @@
 #![no_std]
 #![no_main]
 
+use core::ptr;
+
+use microkit_shared_memory_common::{RegionB, REGION_A_SIZE};
+
 use sel4_microkit::{debug_println, protection_domain, var, Channel, Handler, Infallible};
 
 const CLIENT: Channel = Channel::new(37);
@@ -15,8 +19,8 @@ const CLIENT: Channel = Channel::new(37);
 fn init() -> impl Handler {
     debug_println!("server: initializing");
 
-    let region_a = *var!(region_a_vaddr: usize = 0);
-    let region_b = *var!(region_b_vaddr: usize = 0);
+    let region_a = *var!(region_a_vaddr: usize = 0) as *mut [u8; REGION_A_SIZE];
+    let region_b = *var!(region_b_vaddr: usize = 0) as *mut RegionB;
 
     debug_println!("server: region_a = {region_a:#x?}");
     debug_println!("server: region_b = {region_b:#x?}");
@@ -25,10 +29,27 @@ fn init() -> impl Handler {
 }
 
 struct HandlerImpl {
-    region_a: usize,
-    region_b: usize,
+    region_a: *mut [u8; REGION_A_SIZE],
+    region_b: *mut RegionB,
 }
 
 impl Handler for HandlerImpl {
     type Error = Infallible;
+
+    fn notified(&mut self, _channel: Channel) -> Result<(), Self::Error> {
+        unsafe {
+            assert_eq!((self.region_a as *mut u8).offset(13).read(), 37);
+        }
+
+        unsafe {
+            assert_eq!(
+                (ptr::addr_of!((*self.region_b).foo) as *mut u8)
+                    .offset(1)
+                    .read(),
+                23
+            );
+        }
+
+        Ok(())
+    }
 }
