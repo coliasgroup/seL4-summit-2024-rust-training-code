31a0fd3cf1ff2e40c8f5d21fc376769a20a21a2d
Nick Spinale <nick@nickspinale.com>
2024-10-18 02:13:51 +1100

    11.B

--- workspaces/microkit/shared-memory/test.py
+++ workspaces/microkit/shared-memory/test.py
@@ -8,8 +8,8 @@ from harness import Simulation
 
 with Simulation.from_args() as sim:
     sim.child.expect('server: initializing', timeout=1)
-    sim.child.expect('server: region_a = 0x[0]*2000000', timeout=1)
-    sim.child.expect('server: region_b = 0x[0]*2400000', timeout=1)
+    sim.child.expect('server: region_a = VolatileRef', timeout=1)
+    sim.child.expect('server: region_b = VolatileRef', timeout=1)
     sim.child.expect('client: initializing', timeout=1)
-    sim.child.expect('client: region_a = 0x[0]*2000000', timeout=1)
-    sim.child.expect('client: region_b = 0x[0]*2400000', timeout=1)
+    sim.child.expect('client: region_a = VolatileRef', timeout=1)
+    sim.child.expect('client: region_b = VolatileRef', timeout=1)
