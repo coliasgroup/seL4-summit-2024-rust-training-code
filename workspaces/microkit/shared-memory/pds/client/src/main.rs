db734d5ea476559693be64bbeddf55f81e37fbff
Nick Spinale <nick@nickspinale.com>
2024-10-18 02:13:51 +1100

    11.B

--- workspaces/microkit/shared-memory/pds/client/src/main.rs
+++ workspaces/microkit/shared-memory/pds/client/src/main.rs
@@ -11,7 +11,10 @@ use core::ptr;
 
 use microkit_shared_memory_common::{RegionB, REGION_A_SIZE};
 
-use sel4_microkit::{debug_println, protection_domain, var, Channel, Handler, Infallible};
+use sel4_externally_shared::{map_field, ExternallySharedRef, ExternallySharedRefExt};
+use sel4_microkit::{
+    debug_println, memory_region_symbol, protection_domain, Channel, Handler, Infallible,
+};
 
 const SERVER: Channel = Channel::new(13);
 
@@ -19,28 +22,29 @@ const SERVER: Channel = Channel::new(13);
 fn init() -> impl Handler {
     debug_println!("client: initializing");
 
-    let region_a = *var!(region_a_vaddr: usize = 0) as *mut [u8; REGION_A_SIZE];
-    let region_b = *var!(region_b_vaddr: usize = 0) as *mut RegionB;
+    let mut region_a = unsafe {
+        ExternallySharedRef::new(
+            memory_region_symbol!(region_a_vaddr: *mut [u8], n = REGION_A_SIZE),
+        )
+    };
+
+    let mut region_b =
+        unsafe { ExternallySharedRef::new(memory_region_symbol!(region_b_vaddr: *mut RegionB)) };
 
     debug_println!("client: region_a = {region_a:#x?}");
     debug_println!("client: region_b = {region_b:#x?}");
 
-    unsafe {
-        (region_a as *mut u8).offset(13).write(37);
-    }
+    region_a.as_mut_ptr().index(13).write(37);
 
-    unsafe {
-        (ptr::addr_of!((*region_b).foo) as *mut u8)
-            .offset(1)
-            .write(23);
-    }
+    let region_b_ptr = region_b.as_mut_ptr();
+    map_field!(region_b_ptr.foo).as_slice().index(1).write(23);
 
     HandlerImpl { region_a, region_b }
 }
 
 struct HandlerImpl {
-    region_a: *mut [u8; REGION_A_SIZE],
-    region_b: *mut RegionB,
+    region_a: ExternallySharedRef<'static, [u8]>,
+    region_b: ExternallySharedRef<'static, RegionB>,
 }
 
 impl Handler for HandlerImpl {
